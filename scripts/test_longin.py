7813b4c3dfef28f9849a8d9a0c28e07375e50be1
John Doe <johndoe@example.com>
2018-06-29 17:21:22 +0800

    init commit

---
+++ scripts/test_longin.py
@@ -0,0 +1,16 @@
+class TestLongin:
+    def test_01(self):
+        print('11')
+        assert 1
+
+    def test_02(self):
+        print('22')
+        assert 1
+
+    def test_03(self):
+        print('33')
+        assert 1
+
+    def test_04(self):
+        print('44')
+        assert 0
